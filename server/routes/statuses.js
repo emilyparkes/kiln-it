2816054ca5bbeb9479c4b01074bd754281bd3ba9
emily coco parkes <emilycoco@me.com>
2020-09-29 16:58:51 +1300

    passing status route tests

---
+++ server/routes/statuses.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const db = require('../db/statuses')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.getStatuses()
+    .then(statuses =>
+      res.json({ statuses }))
+    .catch(err => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  db.getStatusById(Number(req.params.id))
+    .then((status) => {
+      if (!status) {
+        return res.status(404).json({
+          error: 'status id not found'
+        })
+      }
+      res.json(status)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+module.exports = router
