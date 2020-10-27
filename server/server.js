29f5f291bc1b7554d7de97591929bb4d18b8cd91
emily coco parkes <emilycoco@me.com>
2020-10-27 15:48:50 +1300

    move public folder

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const creationRoutes = require('./routes/creations')
 
 const server = express()
 
-server.use(express.static(path.join(__dirname, 'public')))
+server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/api/v1/statuses', statusRoutes)
 server.use('/api/v1/creations', creationRoutes)
