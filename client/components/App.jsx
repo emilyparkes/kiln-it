f2c5e3bacd5d1abc554498b79ee15a62c7e5ce2f
emily coco parkes <emilycoco@me.com>
2020-11-06 16:01:51 +1300

    log route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import Nav from './Nav'
+import Nav from './Nav.jsx'
 import Home from './Home'
 import Register from './Register'
 import SignIn from './SignIn'
+import Log from './StatusLog'
 
 export default function App () {
   return (
     <>
       <Route path='/' component={Nav}/>
       <Route exact path='/' component={Home}/>
+      <Route path='/log' component={Log}/>
       <Route path='/register' component={Register}/>
       <Route path='/signin' component={SignIn}/>
+
     </>
   )
 }
