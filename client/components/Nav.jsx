aa93ec1014b10cf7c86caf5d66cd02b908ee2002
emily coco parkes <emilycoco@me.com>
2020-11-18 10:56:23 +1300

    dynamic navlinks

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Nav () {
+export default function Nav (props) {
+  let navLinks = null
+
+  switch (props.location.pathname) {
+    case '/':
+      navLinks = (
+        <>
+          <Link to='/signin'>Sign in</Link>
+          <Link to='/register'>Register</Link>
+        </>
+      )
+      break
+    case '/signin':
+      navLinks = (
+        <>
+          <Link to='/register'>Register</Link>
+          <Link to='/'>Home</Link>
+        </>
+      )
+      break
+    case '/register':
+      navLinks = (
+        <>
+          <Link to='/signin'>Sign in</Link>
+          <Link to='/'>Home</Link>
+        </>
+      )
+      break
+    default:
+      navLinks = (
+        <>
+          <Link to='/signin'>Sign in</Link>
+          <Link to='/register'>Register</Link>
+          <Link to='/'>Home</Link>
+        </>
+      )
+  }
   return (
     <>
-      <Link to='/' >Home</Link>
-      <Link to='/register' >Register</Link>
-      <Link to='/signin' >Sign In</Link>
+      <div className='nav'>
+        <div>{navLinks}</div>
+      </div>
     </>
   )
 }
