1583a49bda9cbc9a249aabe9f984ce56eb4765b1
emily coco parkes <emilycoco@me.com>
2021-01-05 12:15:48 +1300

    correct public path

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ const creationRoutes = require("./routes/creations")
 
 const server = express()
 
-server.use(express.static(path.join(__dirname, "../public")))
+server.use(express.static(path.join(__dirname, "public")))
 
 server.use("/api/v1/statuses", statusRoutes)
 server.use("/api/v1/creations", creationRoutes)
 
 server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
 module.exports = server
