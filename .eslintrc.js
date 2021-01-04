bf106d99b28661cb7328dc530ce2a5774be0ef5d
emily coco parkes <emilycoco@me.com>
2021-01-05 12:06:50 +1300

    index.html and package updates

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+    jest: true,
+  },
+  plugins: ["import", "jest", "node", "prettier", "promise", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/react",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:react/recommended",
+    "plugin:promise/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": { node: { extensions: [".js", ".jsx"] } },
+  },
+  rules: {
+    "react/prop-types": 0,
+  },
+}
