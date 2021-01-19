b94098d0b0ab3a8bd919d2cdd365d178474ebe9a
emily coco parkes <emilycoco@me.com>
2021-01-20 11:55:50 +1300

    logo responsive design

--- client/components/nav/Navigation.jsx
+++ client/components/nav/Navigation.jsx
@@ -6,6 +6,8 @@ const Nav = styled.nav`
   width: 100%;
   height: 55px;
   padding-top: 5px;
+  padding-bottom: 5px;
+  margin-bottom: 10px;
   display: flex;
   .logo {
     font-family: 'Alegreya Sans SC', sans-serif;
@@ -19,17 +21,28 @@ const Nav = styled.nav`
     border-radius: 50%;
     justify-content: left
   }
-  @media (max-width: 768px) {
+  @media (min-width: 250px) {
     .logo {
-      font-family: 'Alegreya Sans SC', sans-serif;
-      flex: 0 0 auto;
-      color: #744F44;
-      padding: 12px 12px 0px 12px;
+      margin-left: 20%;
+      margin-right: auto;
+      font-size: 1.5rem;
+      justify-content: center
+      }
+    
+  }
+  @media (min-width: 480px) {
+    .logo {
       margin-left: auto;
       margin-right: auto;
+      font-size: 1.5rem;
+      justify-content: center
+      }
+  }
+  @media (min-width: 768px) {
+    .logo {
+      margin-left: 10%;
+      margin-right: auto;
       font-size: 2rem;
-      transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-      border-radius: 50%;
       justify-content: center
       }
   }
