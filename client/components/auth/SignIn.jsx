2ecb71eadba8305991900b7463153b020d00a254
emily coco parkes <emilycoco@me.com>
2021-01-26 16:14:05 +1300

    resolve path & remove styled import

--- client/components/auth/SignIn.jsx
+++ client/components/auth/SignIn.jsx
@@ -1,19 +1,4 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-
-const AuthContainer = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  font-family: 'Alegreya Sans SC', sans-serif;
-`
-const AuthImage = styled.div`
-  height: 100%;
-  width: 50%;
-`
-const Form = styled.div`
-  height: 100%;
-  width: 50%;
-`
 
 export default function SignIn () {
   const [username, setUsername] = useState('')
@@ -23,38 +8,33 @@ export default function SignIn () {
 
   return (
     <>
-      <AuthContainer>
-        <AuthImage/>
-        <Form>
-          <div>
-            <h1>Sign in</h1>
-          </div>
+      <div>
+        <h1>Sign in</h1>
+      </div>
 
-          <label>Username</label>
-          <input
-            id='username'
-            name='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder='Username'
-            type='text'
-          />
+      <label>Username</label>
+      <input
+        id='username'
+        name='username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder='Username'
+        type='text'
+      />
 
-          <label>Password</label>
-          <input
-            id='password'
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Password'
-            type='password'
-          />
+      <label>Password</label>
+      <input
+        id='password'
+        name='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='Password'
+        type='password'
+      />
 
-          <button data-testid='submit-button'
-            onClick={handleClick}> Sign in
-          </button>
-        </Form>
-      </AuthContainer>
+      <button data-testid='submit-button'
+        onClick={handleClick}> Sign in
+      </button>
     </>
   )
 }
