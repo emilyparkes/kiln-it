4cdfc48f5c4ada0b183e0ca48687b054a1fcd503
emily coco parkes <emilycoco@me.com>
2021-04-13 16:50:33 +1200

    cards in 2 columns mobile with opacity hover

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Card ({ img, title, date, description }) {
   const [isShown, setIsShown] = useState(false)
 
+  useEffect(() => {
+  }, [isShown])
+
   return (
     <div className='card' >
-      <img className='card-img' src={img}
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      />
-      {isShown
-        ? <div className='item-overlay'></div>
-        : null }
+      <div className='card-overlay v2' text={title} >
+        <img className='card-img' src={img}
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+        />
+      </div>
+
     </div>
   )
 }
+
+// {isShown
+//   ? <div className='card-overlay'>
+//     <h6>{title}</h6>
+//     <p>{date}</p>
+//     <p>{description}</p>
+//   </div>
+//   : null }
