7b92584c36344fd57868da73b569af4ee0bd6b45
emily coco parkes <emilycoco@me.com>
2021-04-28 15:39:15 +1200

    example go of swipe events

--- client/components/Creation.jsx
+++ client/components/Creation.jsx
@@ -57,3 +57,26 @@ export default function Creation (props) {
     </>
   )
 }
+
+// const [touchStart, setTouchStart] = React.useState(0);
+// const [touchEnd, setTouchEnd] = React.useState(0);
+
+// function handleTouchStart(e) {
+//     setTouchStart(e.targetTouches[0].clientX);
+// }
+
+// function handleTouchMove(e) {
+//     setTouchEnd(e.targetTouches[0].clientX);
+// }
+
+// function handleTouchEnd() {
+//     if (touchStart - touchEnd > 150) {
+//         // do stuff here for left swipe
+//         moveSliderRight();
+//     }
+
+//     if (touchStart - touchEnd < -150) {
+//         // do stuff here for right swipe
+//         moveSliderLeft();
+//     }
+// }
