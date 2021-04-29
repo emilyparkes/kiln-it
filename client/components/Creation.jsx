a1ba2c3bd0b52e88bf4d1581e9975f480916cab3
emily coco parkes <emilycoco@me.com>
2021-04-30 11:53:24 +1200

    instagram icon

--- client/components/Creation.jsx
+++ client/components/Creation.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { IoLogoInstagram } from 'react-icons/io'
+
 import { getCreationById } from '../apis/api'
 
 export default function Creation (props) {
@@ -31,27 +33,29 @@ export default function Creation (props) {
     <>
       {creation
         ? <>
-          <img className='creation-img'
-            src={currentImg}/>
+          <div className='creation-container'>
+            <img className='creation-img'
+              src={currentImg}/>
 
-          <div className='icon-dots' >
-            {images.map((dot, idx) => {
-              return <div key={idx}
-                className={imgIdx === idx ? 'dot selected' : 'dot'}
-                id={idx}
-                onClick={() => getImage(idx)}></div>
-            })}
-          </div>
-
-          <div className='text-card'>
-            <div className='text-card-content'>
-              <p className='name'>{creation.name}</p>
-              <p className='shape'>{creation.shape}</p>
-              <p className='description'>{creation.description + ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam cras pharetra magna. Mattis auctor integer nunc adipiscing. Urna tempus integer aliquam ultricies consequat, tempus, aliquam rhoncus, urna. Tortor tellus ac arcu nec, pulvinar. Ut praesent etiam dui, elit non mi viverra nisi. Accumsan commodo neque, in turpis. Morbi in id elit, ultrices donec erat gravida diam.'}</p>
-              <p className='date'>March 12 2021</p>
+            <div className='icon-dots' >
+              {images.map((dot, idx) => {
+                return <div key={idx}
+                  className={imgIdx === idx ? 'dot selected' : 'dot'}
+                  id={idx}
+                  onClick={() => getImage(idx)}></div>
+              })}
             </div>
+
+            <div className='text-card'>
+              <div className='text-card-content'>
+                <p className='name'>{creation.name}</p>
+                <p className='shape'>{creation.shape}</p>
+                <p className='description'>{creation.description + ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam cras pharetra magna. Mattis auctor integer nunc adipiscing. Urna tempus integer aliquam ultricies consequat, tempus, aliquam rhoncus, urna. Tortor tellus ac arcu nec, pulvinar. Ut praesent etiam dui, elit non mi viverra nisi. Accumsan commodo neque, in turpis. Morbi in id elit, ultrices donec erat gravida diam.'}</p>
+                <p className='date'>March 12 2021</p>
+              </div>
+            </div>
+            <IoLogoInstagram className='icon-instagram'/>
           </div>
-          <div className='icon-instagram'>O</div>
         </>
         : 'I\'m sorry no art was made apparently'}
     </>
