457d5c1a3948563e3c9d125bd50c40efdb97d3f2
emily coco parkes <emilycoco@me.com>
2021-05-07 09:27:20 +1200

    styling yay

--- client/components/CreationEdit.jsx
+++ client/components/CreationEdit.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 import { TextField, MenuItem, InputAdornment } from '@material-ui/core'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { brown } from '@material-ui/core/colors'
 import { IoLogoInstagram } from 'react-icons/io'
 import { VscSaveAs } from 'react-icons/vsc'
 
@@ -20,6 +22,12 @@ export default function CreationEdit (props) {
   const clays = ['White', 'Red', 'White Speckle']
   const glazes = ['Black Matte', 'White', 'White Speckle']
   //
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: brown
+    }
+  })
 
   useEffect(() => {
     getCreationById(1)
@@ -83,131 +91,133 @@ export default function CreationEdit (props) {
             <div className='text-card'>
               <div className='text-card-content'>
                 <div className={classes.root}>
+                  <ThemeProvider theme={theme}>
 
-                  <TextField label='Name'
-                    className={classes.name}
-                    variant='outlined'
-                    size='small'
-                    id='outlined-name'
-                    margin="dense"
-                    name='name'
-                    value={form.name}
-                    onChange={handleChange}
-                  />
+                    <TextField label='Name'
+                      className={classes.name}
+                      variant='outlined'
+                      size='small'
+                      id='outlined-name'
+                      margin='dense'
+                      name='name'
+                      value={form.name}
+                      onChange={handleChange}
+                    />
 
-                  <TextField label='Shape'
-                    className={classes.textField}
-                    variant='outlined'
-                    size='small'
-                    id='outlined-shape'
-                    margin="dense"
-                    select
-                    name='shape'
-                    value={form.shape}
-                    onChange={handleChange}
-                  >
-                    {shapes.map((shape) => (
-                      <MenuItem key={shape} value={shape}>
-                        {shape}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                    <TextField label='Shape'
+                      className={classes.textField}
+                      variant='outlined'
+                      size='small'
+                      id='outlined-shape'
+                      margin='dense'
+                      select
+                      name='shape'
+                      value={form.shape}
+                      onChange={handleChange}
+                    >
+                      {shapes.map((shape) => (
+                        <MenuItem key={shape} value={shape}>
+                          {shape}
+                        </MenuItem>
+                      ))}
+                    </TextField>
 
-                  <TextField label='Status'
-                    className={classes.textField}
-                    variant='outlined'
-                    size='small'
-                    id='outlined-status'
-                    margin="dense"
-                    select
-                    name='status'
-                    value={form.status}
-                    onChange={handleChange}
-                  >
-                    {statuses.map((status) => (
-                      <MenuItem key={status} value={status}>
-                        {status}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                    <TextField label='Status'
+                      className={classes.textField}
+                      variant='outlined'
+                      size='small'
+                      id='outlined-status'
+                      margin='dense'
+                      select
+                      name='status'
+                      value={form.status}
+                      onChange={handleChange}
+                    >
+                      {statuses.map((status) => (
+                        <MenuItem key={status} value={status}>
+                          {status}
+                        </MenuItem>
+                      ))}
+                    </TextField>
 
-                  <TextField label='Clay'
-                    className={classes.textField}
-                    variant='outlined'
-                    size='small'
-                    id='outlined-clay'
-                    margin="dense"
-                    select
-                    name='clay'
-                    value={form.clay}
-                    onChange={handleChange}
-                  >
-                    {clays.map((clay) => (
-                      <MenuItem key={clay} value={clay}>
-                        {clay}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                    <TextField label='Clay'
+                      className={classes.textField}
+                      variant='outlined'
+                      size='small'
+                      id='outlined-clay'
+                      margin='dense'
+                      select
+                      name='clay'
+                      value={form.clay}
+                      onChange={handleChange}
+                    >
+                      {clays.map((clay) => (
+                        <MenuItem key={clay} value={clay}>
+                          {clay}
+                        </MenuItem>
+                      ))}
+                    </TextField>
 
-                  <TextField label='Weight'
-                    className={classes.textField}
-                    variant='outlined'
-                    size='small'
-                    id='outlined-weight'
-                    margin="dense"
-                    InputProps={{
-                      endAdornment: <InputAdornment position="end">g</InputAdornment>
-                    }}
-                    name='weight'
-                    value={form.weight}
-                    onChange={handleChange}
-                  />
+                    <TextField label='Weight'
+                      className={classes.textField}
+                      variant='outlined'
+                      size='small'
+                      id='outlined-weight'
+                      margin='dense'
+                      InputProps={{
+                        endAdornment: <InputAdornment position='end'>g</InputAdornment>
+                      }}
+                      name='weight'
+                      value={form.weight}
+                      onChange={handleChange}
+                    />
 
-                  <TextField label='Glaze'
-                    variant='outlined'
-                    size='small'
-                    id='outlined-glaze'
-                    margin="dense"
-                    select
-                    fullWidth
-                    name='glaze'
-                    value={form.glaze}
-                    onChange={handleChange}
-                  >
-                    {glazes.map((glaze) => (
-                      <MenuItem key={glaze} value={glaze}>
-                        {glaze}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                    <TextField label='Glaze'
+                      variant='outlined'
+                      size='small'
+                      id='outlined-glaze'
+                      margin='dense'
+                      select
+                      fullWidth
+                      name='glaze'
+                      value={form.glaze}
+                      onChange={handleChange}
+                    >
+                      {glazes.map((glaze) => (
+                        <MenuItem key={glaze} value={glaze}>
+                          {glaze}
+                        </MenuItem>
+                      ))}
+                    </TextField>
 
-                  <TextField label='Makers Note'
-                    variant='outlined'
-                    size='small'
-                    id='outlined-makers-note'
-                    margin="dense"
-                    multiline
-                    rows={5}
-                    fullWidth
-                    name='makersNote'
-                    value={form.makersNote}
-                    onChange={handleChange}
-                  />
+                    <TextField label='Makers Note'
+                      variant='outlined'
+                      size='small'
+                      id='outlined-makers-note'
+                      margin='dense'
+                      multiline
+                      rows={5}
+                      fullWidth
+                      name='makersNote'
+                      value={form.makersNote}
+                      onChange={handleChange}
+                    />
 
-                  <TextField label='Description'
-                    variant='outlined'
-                    size='small'
-                    id='outlined-multiline-static'
-                    margin="dense"
-                    multiline
-                    rows={8}
-                    fullWidth
-                    name='description'
-                    value={form.description}
-                    onChange={handleChange}
-                  />
+                    <TextField label='Description'
+                      variant='outlined'
+                      size='small'
+                      id='outlined-multiline-static'
+                      margin='dense'
+                      multiline
+                      rows={8}
+                      fullWidth
+                      name='description'
+                      value={form.description}
+                      onChange={handleChange}
+                    />
 
-                  <p className='date'>March 12 2021</p>
+                    <p className='date'>March 12 2021</p>
+                  </ThemeProvider>
                 </div>
 
               </div>
