4ec6c99a7b8cd60f9495d946143c9ae13cb6df6a
emily coco parkes <emilycoco@me.com>
2021-05-10 15:34:27 +1200

    model with editable statuses

--- client/components/StatusLogItem.jsx
+++ client/components/StatusLogItem.jsx
@@ -1,13 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import StatusModal from './StatusModal'
+
+const statuses = ['Wet', 'Leather Hard', 'Bone Dry', 'Bisque Firing', 'Bisque Fired', 'Glazed', 'Glaze Firing', 'Complete']
 
 export default function StatusLogItem ({ creation }) {
-  const style = creation.status.toLowerCase().replace(/\s/g, '-')
+  const [show, setShowModel] = useState(false)
+  const [currentStatus, setStatus] = useState(creation.status)
+
+  const style = currentStatus.toLowerCase().replace(/\s/g, '-')
+
+  useEffect(() => {
+    show
+      ? document.body.style.overflow = 'hidden'
+      : document.body.style.overflow = ''
+  }, [show, currentStatus])
+
+  const showModal = () => {
+    setShowModel(true)
+  }
+
+  const hideModal = () => {
+    setShowModel(false)
+  }
+
+  const handleSelect = (e) => {
+    setStatus(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    // e.preventDefault()
+    hideModal()
+    // sendForm(form)
+  }
+
   return (
-    <div className='log' key={creation.id}>
-      {/* Img */}
-      <div>{creation.shape}</div>
-      <div>{creation.date_created}</div>
-      <div className={`status ${style}`} >{creation.status}</div>
-    </div>
+    <>
+      {show
+        ? <main className='main edit'>
+          <StatusModal show={show} save={onSubmit} close={hideModal}>
+            <div className='current'>
+              <p>Selected</p>
+              <p className={`status ${style}`}>
+                {currentStatus}
+              </p>
+            </div>
+            <div className='statusList'>
+              {statuses.map((status) => {
+                const styleItem = status.toLowerCase().replace(/\s/g, '-')
+
+                return <button className={`status ${styleItem}`}
+                  key={status} value={status}
+                  onClick={handleSelect}>
+                  {status}
+                </button>
+              })}
+            </div>
+          </StatusModal>
+        </main>
+        : null}
+
+      <div className='item'>
+        <div className={`status ${style}`}
+          onClick={showModal}>
+          {currentStatus}
+        </div>
+
+        <Link to='/creations/le-vase'>
+          <div className='log-box' key={creation.id}>
+
+            <img className='log-img'
+              src='/images/plate.jpeg'/>
+
+            <div className='info'>
+              <div>{creation.shape}</div>
+              <div>{creation.date_created}</div>
+            </div>
+
+          </div>
+        </Link>
+
+      </div>
+    </>
   )
 }
---
+++ client/components/StatusModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function StatusModal ({ save, close, show, children }) {
+  const showHideClassName = show ? 'modal open' : 'modal closed'
+
+  const handleSave = () => {
+    // save()
+    close()
+  }
+
+  return (
+    <div className={showHideClassName}>
+      <section className='model-box'>
+        {children}
+        <button className='save' type='button' onClick={handleSave}>
+          Save
+        </button>
+      </section>
+    </div>
+  )
+}
