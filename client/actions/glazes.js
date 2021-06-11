fbcdc92d7abad86d5d8e8d71a63e82a9bc2fc1ab
emily coco parkes <emilycoco@me.com>
2021-06-11 15:06:35 +1200

    glazes actions and api

--- client/actions/glazes.js
+++ client/actions/glazes.js
@@ -1,28 +1,82 @@
 import { showError } from './error'
-import { getGlazes } from '../apis/glazes'
+import { getGlazes, addGlazes, updateGlaze, deleteGlaze } from '../apis/glazes'
 
-const GET_GLAZES_PENDING = 'GET_GLAZES_PENDING'
-const RECEIVE_GLAZES = 'RECEIVE_GLAZES'
+export const GLAZES_REQUEST_PENDING = 'GLAZES_REQUEST_PENDING'
+export const RECEIVE_GLAZES = 'RECEIVE_GLAZES'
+export const ADD_NEW_GLAZES = 'ADD_NEW_GLAZES'
+export const UPDATE_GLAZE = 'UPDATE_GLAZE'
+export const REMOVE_GLAZE = 'REMOVE_GLAZE'
 
-export function getGlazesPending () {
+export function requestGlazesPending () {
   return {
-    type: GET_GLAZES_PENDING
+    type: GLAZES_REQUEST_PENDING
   }
 }
 
-export function receiveGlazes (glazes) {
+export function receiveGlazesSuccess (glazes) {
   return {
     type: RECEIVE_GLAZES,
     glazes
   }
 }
 
+export function newGlazesSuccess (glazes) {
+  return {
+    type: ADD_NEW_GLAZES,
+    glazes
+  }
+}
+
+export function updateGlazeSuccess (glaze) {
+  return {
+    type: UPDATE_GLAZE,
+    glaze
+  }
+}
+
+export function removeGlazeSuccess (id) {
+  return {
+    type: REMOVE_GLAZE,
+    id
+  }
+}
+
 export function fetchGlazes () {
   return dispatch => {
-    dispatch(getGlazesPending())
+    dispatch(requestGlazesPending())
 
     getGlazes()
-      .then(glazes => dispatch(receiveGlazes(glazes)))
+      .then(glazes => dispatch(receiveGlazesSuccess(glazes)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function createGlazes (glazes) {
+  return dispatch => {
+    dispatch(requestGlazesPending())
+
+    addGlazes(glazes)
+      .then(glazes => dispatch(newGlazesSuccess(glazes)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function modifyGlaze (glaze) {
+  return dispatch => {
+    dispatch(requestGlazesPending())
+
+    updateGlaze(glaze)
+      .then((glaze) => dispatch(updateGlazeSuccess(glaze)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function removeGlaze (id) {
+  return dispatch => {
+    dispatch(requestGlazesPending())
+
+    deleteGlaze(id)
+      .then(() => dispatch(removeGlazeSuccess(id)))
       .catch(err => dispatch(showError(err.message)))
   }
 }
--- client/apis/glazes.js
+++ client/apis/glazes.js
@@ -8,8 +8,23 @@ export function getGlazes () {
   })
 }
 
-export function addNewGlaze (glazes) {
+export function addGlazes (glazes) {
   return request.post(baseUrl).send(glazes)
+    .then((resp) => {
+      console.log('api: ', resp.body)
+      return resp.body.glazes
+    })
+}
+
+export function updateGlaze (id, glaze) {
+  return request.patch(`${baseUrl}/${id}`).send(glaze)
+    .then((resp) => {
+      return resp.body
+    })
+}
+
+export function deleteGlaze (id) {
+  return request.delete(`${baseUrl}/${id}`)
     .then((resp) => {
       return resp.body
     })
