f77c40adf86212f1cb1574e46badc81715dd973b
emily coco parkes <emilycoco@me.com>
2021-06-11 15:07:05 +1200

    status actions and api

--- client/actions/statuses.js
+++ client/actions/statuses.js
@@ -1,12 +1,15 @@
 import { showError } from './error'
-import { getStatuses } from '../apis/statuses'
+import { getStatuses, addStatuses, updateStatus, deleteStatus } from '../apis/statuses'
 
-const GET_STATUSES_PENDING = 'GET_STATUSES_PENDING'
-const RECEIVE_STATUSES = 'RECEIVE_STATUSES'
+export const STATUSES_REQUEST_PENDING = 'STATUSES_REQUEST_PENDING'
+export const RECEIVE_STATUSES = 'RECEIVE_STATUSES'
+export const ADD_NEW_STATUSES = 'ADD_NEW_STATUSES'
+export const UPDATE_STATUS = 'UPDATE_STATUS'
+export const REMOVE_STATUS = 'REMOVE_STATUS'
 
-export function getStatusesPending () {
+export function requestStatusesPending () {
   return {
-    type: GET_STATUSES_PENDING
+    type: STATUSES_REQUEST_PENDING
   }
 }
 
@@ -17,12 +20,63 @@ export function receiveStatuses (statuses) {
   }
 }
 
+export function newStatusesSuccess (statuses) {
+  return {
+    type: ADD_NEW_STATUSES,
+    statuses
+  }
+}
+
+export function updateStatusSuccess (status) {
+  return {
+    type: UPDATE_STATUS,
+    status
+  }
+}
+
+export function removeStatusSuccess (id) {
+  return {
+    type: REMOVE_STATUS,
+    id
+  }
+}
+
 export function fetchStatuses () {
   return dispatch => {
-    dispatch(getStatusesPending())
+    dispatch(requestStatusesPending())
 
     getStatuses()
       .then(statuses => dispatch(receiveStatuses(statuses)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function createStatuses (statuses) {
+  return dispatch => {
+    dispatch(requestStatusesPending())
+
+    addStatuses(statuses)
+      .then(statuses => dispatch(newStatusesSuccess(statuses)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function modifyStatus (status) {
+  return dispatch => {
+    dispatch(requestStatusesPending())
+
+    updateStatus(status)
+      .then((status) => dispatch(updateStatusSuccess(status)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function removeStatus (id) {
+  return dispatch => {
+    dispatch(requestStatusesPending())
+
+    deleteStatus(id)
+      .then(() => dispatch(removeStatusSuccess(id)))
       .catch(err => dispatch(showError(err.message)))
   }
 }
--- client/apis/statuses.js
+++ client/apis/statuses.js
@@ -8,8 +8,22 @@ export function getStatuses () {
   })
 }
 
-export function addNewStatus (statuses) {
+export function addStatuses (statuses) {
   return request.post(baseUrl).send(statuses)
+    .then((resp) => {
+      return resp.body.statuses
+    })
+}
+
+export function updateStatus (id, status) {
+  return request.patch(`${baseUrl}/${id}`).send(status)
+    .then((resp) => {
+      return resp.body
+    })
+}
+
+export function deleteStatus (id) {
+  return request.delete(`${baseUrl}/${id}`)
     .then((resp) => {
       return resp.body
     })
