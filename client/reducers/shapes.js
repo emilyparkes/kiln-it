4d606e48a7b9dd178784f68c6728d313ca1435eb
emily coco parkes <emilycoco@me.com>
2021-06-11 15:08:52 +1200

    shapes reducer to update state

--- client/reducers/shapes.js
+++ client/reducers/shapes.js
@@ -1,9 +1,16 @@
-const initialState = null
+import { RECEIVE_SHAPES, ADD_NEW_SHAPES, UPDATE_SHAPE, REMOVE_SHAPE } from '../actions/shapes'
 
+const initialState = null
 export default function shapes (state = initialState, action) {
   switch (action.type) {
-    case 'RECEIVE_SHAPES':
+    case RECEIVE_SHAPES:
       return action.shapes
+    case ADD_NEW_SHAPES:
+      return state ? [...state, ...action.shapes] : action.shapes
+    case UPDATE_SHAPE:
+      return state.map(shape => shape.id === action.shape.id ? action.shape : shape)
+    case REMOVE_SHAPE:
+      return state.filter(shape => shape.id !== action.id)
     default:
       return state
   }
