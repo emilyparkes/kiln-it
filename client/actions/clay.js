96cbe174e6910f557ad37084042c495a1f7cd8b1
emily coco parkes <emilycoco@me.com>
2021-06-11 15:04:42 +1200

    clay actions and api calls

--- client/actions/clay.js
+++ client/actions/clay.js
@@ -1,28 +1,82 @@
 import { showError } from './error'
-import { getClay } from '../apis/clay'
+import { getClay, addClay, updateClay, deleteClay } from '../apis/clay'
 
-const GET_CLAY_PENDING = 'GET_CLAY_PENDING'
-const RECEIVE_CLAY = 'RECEIVE_CLAY'
+export const CLAY_REQUEST_PENDING = 'CLAY_REQUEST_PENDING'
+export const RECEIVE_CLAY = 'RECEIVE_CLAY'
+export const ADD_NEW_CLAY = 'ADD_NEW_CLAY'
+export const UPDATE_CLAY = 'UPDATE_CLAY'
+export const REMOVE_CLAY = 'REMOVE_CLAY'
 
-export function getClayPending () {
+export function requestClayPending () {
   return {
-    type: GET_CLAY_PENDING
+    type: CLAY_REQUEST_PENDING
   }
 }
 
-export function receiveClay (clay) {
+export function receiveClaySuccess (clay) {
   return {
     type: RECEIVE_CLAY,
     clay
   }
 }
 
+export function newClaySuccess (clay) {
+  return {
+    type: ADD_NEW_CLAY,
+    clay
+  }
+}
+
+export function updateClaySuccess (clay) {
+  return {
+    type: UPDATE_CLAY,
+    clay
+  }
+}
+
+export function removeClaySuccess (id) {
+  return {
+    type: REMOVE_CLAY,
+    id
+  }
+}
+
 export function fetchClay () {
   return dispatch => {
-    dispatch(getClayPending())
+    dispatch(requestClayPending())
 
     getClay()
-      .then(clay => dispatch(receiveClay(clay)))
+      .then(clay => dispatch(receiveClaySuccess(clay)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function createClay (clay) {
+  return dispatch => {
+    dispatch(requestClayPending())
+
+    addClay(clay)
+      .then(clay => dispatch(newClaySuccess(clay)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function modifyClay (clay) {
+  return dispatch => {
+    dispatch(requestClayPending())
+
+    updateClay(clay)
+      .then((clay) => dispatch(updateClaySuccess(clay)))
+      .catch(err => dispatch(showError(err.message)))
+  }
+}
+
+export function removeClay (id) {
+  return dispatch => {
+    dispatch(requestClayPending())
+
+    deleteClay(id)
+      .then(() => dispatch(removeClaySuccess(id)))
       .catch(err => dispatch(showError(err.message)))
   }
 }
--- client/apis/clay.js
+++ client/apis/clay.js
@@ -8,8 +8,22 @@ export function getClay () {
   })
 }
 
-export function addNewClay (clay) {
+export function addClay (clay) {
   return request.post(baseUrl).send(clay)
+    .then((resp) => {
+      return resp.body.clay
+    })
+}
+
+export function updateClay (id, clay) {
+  return request.patch(`${baseUrl}/${id}`).send(clay)
+    .then((resp) => {
+      return resp.body
+    })
+}
+
+export function deleteClay (id) {
+  return request.delete(`${baseUrl}/${id}`)
     .then((resp) => {
       return resp.body
     })
