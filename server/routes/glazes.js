19085085793d7fbc578d5a1b48d809743761fef5
emily coco parkes <emilycoco@me.com>
2021-06-11 15:10:35 +1200

    update glazes routes add crud routes

--- server/routes/glazes.js
+++ server/routes/glazes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const db = require('../db/glazes')
-const { prepForDb, prepForJS } = require('../server-utils')
+// const { prepForDb, prepForJS } = require('../server-utils')
 
 const router = express.Router()
 
@@ -16,13 +16,34 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const addedGlazes = req.body.map(glaze => {
-    return db.addNewGlaze(glaze)
+    return db.addGlaze(glaze)
       .then((id) => {
         return { id: id[0], glaze }
       })
   })
   Promise.all(addedGlazes)
-    .then((glazes) => res.json({ glazes }))
+    .then((glazes) => {
+      console.log(glazes)
+      return res.json({ glazes })
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  return db.updateGlaze(Number(req.params.id), req.body.glaze)
+    .then((glaze) => res.json({ glaze }))
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  return db.deleteGlaze(Number(req.params.id))
+    .then((deleted) => res.json({ deleted: `${deleted} item(s) have been deleted successfully` }))
     .catch((err) => {
       console.error(err)
       res.sendStatus(500)
