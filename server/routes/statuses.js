5f65588df238603aaae479ae0ab26e214c4e282f
emily coco parkes <emilycoco@me.com>
2021-06-11 15:11:06 +1200

    update status routes add crud routes

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -1,49 +1,13 @@
 const express = require('express')
 
 const db = require('../db/statuses')
-const { prepForDb, prepForJS } = require('../server-utils')
+// const { prepForDb, prepForJS } = require('../server-utils')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getStatuses()
+  return db.getStatuses()
     .then((statuses) => res.json({ statuses }))
-    .catch((err) => {
-      console.error(err)
-      res.sendStatus(500)
-    })
-})
-
-router.get('/:id', (req, res) => {
-  db.getStatusById(Number(req.params.id))
-    .then((status) => {
-      if (!status) {
-        return res.status(404).json({
-          error: 'status id not found'
-        })
-      }
-      res.json(status)
-      return null
-    })
-    .catch((err) => {
-      console.error(err)
-      res.sendStatus(500)
-    })
-})
-
-router.patch('/:id', (req, res) => {
-  const creation = prepForDb(req.body)
-  db.updateCreationById(Number(req.params.id), creation)
-    .then((creation) => {
-      if (!creation) {
-        return res.status(404).json({
-          error: 'creation id not found'
-        })
-      }
-      creation = prepForJS(creation)
-      res.json(creation)
-      return null
-    })
     .catch((err) => {
       console.error(err)
       res.sendStatus(500)
@@ -52,7 +16,7 @@ router.patch('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const addedStatuses = req.body.map(status => {
-    return db.addNewStatus(status)
+    return db.addStatus(status)
       .then((id) => {
         return { id: id[0], status }
       })
@@ -64,4 +28,23 @@ router.post('/', (req, res) => {
       res.sendStatus(500)
     })
 })
+
+router.patch('/:id', (req, res) => {
+  return db.updateStatus(Number(req.params.id), req.body.status)
+    .then((status) => res.json({ status }))
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  return db.deleteStatus(Number(req.params.id))
+    .then((deleted) => res.json({ deleted: `${deleted} item(s) have been deleted successfully` }))
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
 module.exports = router
