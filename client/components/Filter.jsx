4fd8ea3e09eec85d3a8e39eec32906aab91731f3
emily coco parkes <emilycoco@me.com>
2021-07-13 21:48:19 +1200

    filter mockup

---
+++ client/components/Filter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function Filter (props) {
+  return (
+    <div>
+      <div>Filter Logo</div>
+      <div>Current Filter</div>
+      {/* opens modal? of options to choose */}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filter
+  }
+}
+
+export default connect(mapStateToProps)(Filter)
