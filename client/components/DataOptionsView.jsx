ecc697db341cd163661db33892abf0fbd9e423a7
emily coco parkes <emilycoco@me.com>
2021-07-13 20:55:28 +1200

    clean component

--- client/components/DataOptionsView.jsx
+++ client/components/DataOptionsView.jsx
@@ -4,24 +4,16 @@ import { connect } from 'react-redux'
 import DataOption from './DataOption'
 
 function AddDataOptions ({ clay, glazes, shapes, statuses, dispatch }) {
-  // const addItem = () => {
-
-  // }
-
-  // const delItem = () => {
-
-  // }
-
   return (
     <>
       { (statuses && clay && glazes && shapes) &&
         <div className='container'>
           <h3 className='pg-title heading'>Manage options</h3>
           <div className='options'>
-            <DataOption name='clay' arrOfType={clay} dispatch={dispatch}/>
-            <DataOption name='glaze' arrOfType={glazes} dispatch={dispatch}/>
-            <DataOption name='shape' arrOfType={shapes} dispatch={dispatch}/>
-            <DataOption name='status' arrOfType={statuses} dispatch={dispatch}/>
+            <DataOption name='clay' arrOfType={clay} dispatch={dispatch} />
+            <DataOption name='glaze' arrOfType={glazes} dispatch={dispatch} />
+            <DataOption name='shape' arrOfType={shapes} dispatch={dispatch} />
+            <DataOption name='status' arrOfType={statuses} dispatch={dispatch} />
           </div>
         </div>
       }
