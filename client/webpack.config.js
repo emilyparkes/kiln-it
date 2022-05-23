6ccecaee45deb00c066373a7a1c19d371a1e6eb8
emily parkes <emilycoco@me.com>
2022-05-24 10:51:59 +1200

    update to heroku 20

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,16 +32,18 @@ module.exports = [
   {
     mode: 'production',
     client: 'postgresql',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    module: {
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
     }
   }
 ]
