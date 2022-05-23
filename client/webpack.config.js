981341c7d9aad1d3a02411d54a0f0034e3c61402
emily parkes <emilycoco@me.com>
2022-05-24 11:41:50 +1200

    put config in correct place

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,49 +1,30 @@
 const path = require('path')
 
-module.exports = [
-  {
-    entry: [
-      path.join(__dirname, 'index.js'),
-      path.join(__dirname, 'styles/index.scss')
-    ],
-    output: {
-      path: path.join(__dirname, '../server/public'),
-      filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.(sa|sc|c)ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devtool: 'source-map'
+module.exports = {
+  entry: [
+    path.join(__dirname, 'index.js'),
+    path.join(__dirname, 'styles/index.scss')
+  ],
+  output: {
+    path: path.join(__dirname, '../server/public'),
+    filename: 'bundle.js'
   },
-  {
-    mode: 'production',
-    client: 'postgresql',
-    module: {
-      connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
-    }
-  }
-]
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -31,13 +31,9 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: path.join(__dirname, 'migrations')
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     }
   },
 
