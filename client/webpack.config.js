525777c357af806525279c11c482a81b23dca027
emily parkes <emilycoco@me.com>
2022-05-24 10:44:17 +1200

    alter webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,33 +1,36 @@
 const path = require('path')
 
-module.exports = {
-  entry: [
-    path.join(__dirname, 'index.js'),
-    path.join(__dirname, 'styles/index.scss')
-  ],
-  output: {
-    path: path.join(__dirname, '../server/public'),
-    filename: 'bundle.js'
+module.exports = [
+  {
+    entry: [
+      path.join(__dirname, 'index.js'),
+      path.join(__dirname, 'styles/index.scss')
+    ],
+    output: {
+      path: path.join(__dirname, '../server/public'),
+      filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map'
   },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devtool: 'source-map',
-  production: {
+  {
+    mode: 'production',
     client: 'postgresql',
     connection: {
       connectionString: process.env.DATABASE_URL,
@@ -41,4 +44,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-}
+]
