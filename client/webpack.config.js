b73de9392ead91e5632641acb0ba850fa1934a29
emily parkes <emilycoco@me.com>
2022-05-24 10:32:20 +1200

    update to heroku 20

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,5 +26,19 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const server = require('./server')
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
+
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
