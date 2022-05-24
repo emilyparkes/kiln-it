4f96f9e6af0c27f3c2607f07d07506d1b1347de3
emily parkes <emilycoco@me.com>
2022-05-25 10:19:40 +1200

    update test data

--- server/db/tests/seeds/5_shapes.js
+++ server/db/tests/seeds/5_shapes.js
@@ -2,13 +2,13 @@ exports.seed = (knex) => {
   return knex('shapes')
     .then(() => {
       return knex('shapes').insert([
-        { id: 1, shape: 'Coffee Cup' },
-        { id: 2, shape: 'Plate' },
-        { id: 3, shape: 'Bowl' },
-        { id: 4, shape: 'Paint Palette' },
-        { id: 5, shape: 'Vase' },
-        { id: 6, shape: 'Planter' },
-        { id: 7, shape: 'Artistic' }
+        { id: 1, shape: 'Coffee Cup', in_use: true },
+        { id: 2, shape: 'Plate', in_use: true },
+        { id: 3, shape: 'Bowl', in_use: true },
+        { id: 4, shape: 'Paint Palette', in_use: true },
+        { id: 5, shape: 'Vase', in_use: true },
+        { id: 6, shape: 'Planter', in_use: true },
+        { id: 7, shape: 'Artistic', in_use: true }
       ])
     })
 }
