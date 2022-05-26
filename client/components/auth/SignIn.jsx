8647beea29c5d08fecd2d5bc2138da2ef185bff4
emily parkes <emilycoco@me.com>
2022-05-27 10:08:15 +1200

    sign in

--- client/components/auth/SignIn.jsx
+++ client/components/auth/SignIn.jsx
@@ -1,41 +1,85 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { TextField } from '@material-ui/core'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { brown } from '@material-ui/core/colors'
+import { useEditStyles } from '../../styles/mui_overrides'
+
+import { signIn } from '../../apis/firebase/auth'
 
 function SignIn () {
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [formErr, setFormErr] = useState('')
 
-  const handleClick = () => {}
+  const dispatch = useDispatch()
+  const classes = useEditStyles()
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: brown
+    }
+  })
+
+  const handleClick = () => {
+    signIn(email, password, setFormErr)
+      .then(user => dispatch(user))
+      .catch(err => console.log(err.message))
+  }
 
   return (
     <>
-      <div>
-        <h1>Sign in</h1>
-      </div>
+      {formErr}
+      <ThemeProvider theme={theme}>
+        <div>
+          <h1>Sign in</h1>
+        </div>
 
-      <label>Username</label>
-      <input
-        id='username'
-        name='username'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder='Username'
-        type='text'
-      />
+        <TextField label='Email'
+          className={classes.name}
+          variant='outlined'
+          size='small'
+          id='outlined-email'
+          margin='dense'
+          name='name'
+          // value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='name@email.com'
+          type='email'
+        />
 
-      <label>Password</label>
-      <input
-        id='password'
-        name='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder='Password'
-        type='password'
-      />
+        <TextField label='Password'
+          className={classes.name}
+          variant='outlined'
+          size='small'
+          id='outlined-password'
+          margin='dense'
+          name='name'
+          // value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='password'
+          type='password'
+        />
 
-      <button data-testid='submit-button'
-        onClick={handleClick}> Sign in
-      </button>
+        <button data-testid='submit-button'
+          onClick={handleClick}> Sign in
+        </button>
+      </ThemeProvider>
     </>
   )
 }
 export default SignIn
+
+// const auth = getAuth();
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
