fa6c361915f8834993e20132b761fd20ec2f3fa3
emily parkes <emilycoco@me.com>
2022-05-27 10:07:01 +1200

    register

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -1,46 +1,90 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { TextField } from '@material-ui/core'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { brown } from '@material-ui/core/colors'
+import { useEditStyles } from '../../styles/mui_overrides'
+
+import { register } from '../../apis/firebase/auth'
 
 function Register () {
-  const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [formErr, setFormErr] = useState('')
 
-  const handleClick = () => {}
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const classes = useEditStyles()
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: brown
+    }
+  })
+
+  const handleClick = () => {
+    register(email, password, setFormErr)
+      // eslint-disable-next-line promise/always-return
+      .then(user => {
+        dispatch(user)
+        navigate('/gallery')
+      })
+      .catch(err => console.log(err.message))
+  }
 
   return (
     <>
-      <div className='registerTitle'>
-        <h1>Register</h1>
-      </div>
+      {formErr}
+      <ThemeProvider theme={theme}>
+        <div className='registerTitle'>
+          <h1>Register</h1>
+        </div>
 
-      <label className='label'>Username</label>
-      <input
-        type='username'
-        value={username}
-        placeholder='username'
-        onChange={(e) => setUsername(e.target.value)}
-      ></input>
+        <TextField label='Email'
+          className={classes.name}
+          variant='outlined'
+          size='small'
+          id='outlined-email'
+          margin='dense'
+          name='name'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='kelly@clay.com'
+          type='email'
+        />
 
-      <label>Email</label>
-      <input
-        id='email'
-        name='email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder='email'
-      />
+        <TextField label='Password'
+          className={classes.name}
+          variant='outlined'
+          size='small'
+          id='outlined-password'
+          margin='dense'
+          name='name'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='password'
+          type='password'
+        />
+        <TextField label='Confirm Password'
+          className={classes.name}
+          variant='outlined'
+          size='small'
+          id='outlined-password-confirm'
+          margin='dense'
+          name='name'
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          placeholder='Re-type password'
+          type='password'
+        />
 
-      <label className='label'>Password</label>
-      <input
-        type='password'
-        value={password}
-        placeholder='password'
-        onChange={(e) => setPassword(e.target.value)}
-      ></input>
-
-      <button type='button' onClick={handleClick}>
+        <button type='button' onClick={handleClick}>
         Register
-      </button>
+        </button>
+      </ThemeProvider>
     </>
   )
 }
