2d0fba733c980779118d7a018d3cb662be4facf9
emily parkes <emilycoco@me.com>
2022-08-02 16:34:38 +1200

    fix duplicate variables

--- client/components/DataOptionsView.jsx
+++ client/components/DataOptionsView.jsx
@@ -5,10 +5,10 @@ import DataOption from './DataOption'
 
 function AddDataOptions () {
 
-const clay = useSelector(store => store.clay)
-const glazes = useSelector(store => store.glazes)
-const shapes = useSelector(store => store.shapes)
-const statuses = useSelector(store => store.statuses)
+  const clay = useSelector(store => store.clay)
+  const glazes = useSelector(store => store.glazes)
+  const shapes = useSelector(store => store.shapes)
+  const statuses = useSelector(store => store.statuses)
 
   return (
     <>
@@ -16,10 +16,10 @@ const statuses = useSelector(store => store.statuses)
         <div className='container'>
           <h3 className='pg-title heading'>Manage options</h3>
           <div className='options'>
-            <DataOption name='clay' arrOfType={clay} dispatch={dispatch} />
-            <DataOption name='glaze' arrOfType={glazes} dispatch={dispatch} />
-            <DataOption name='shape' arrOfType={shapes} dispatch={dispatch} />
-            <DataOption name='status' arrOfType={statuses} dispatch={dispatch} />
+            <DataOption name='clay' arrOfType={clay} />
+            <DataOption name='glaze' arrOfType={glazes} />
+            <DataOption name='shape' arrOfType={shapes} />
+            <DataOption name='status' arrOfType={statuses} />
           </div>
         </div>
       }
