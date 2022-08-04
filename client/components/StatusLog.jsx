2afe7f49a5cddbcc0ad139e81263e900d8be76bb
emily parkes <emilycoco@me.com>
2022-08-05 11:07:14 +1200

    render based on filter or search active

--- client/components/StatusLog.jsx
+++ client/components/StatusLog.jsx
@@ -3,18 +3,21 @@ import { useSelector } from 'react-redux'
 
 import NavUtils from './nav-utils/NavUtils'
 import StatusLogItem from './StatusLogItem'
+
 import { filterBy } from '../client-utils'
 
 function StatusLog () {
-
-  const creations = useSelector(store => filterBy(store.filter, store.creations))
+  const focus = useSelector(store => store.navUtils)
+  const creationsFiltered = useSelector(store => filterBy(store.filter, store.creations))
+  // const creationsSearched = useSelector(store => searchBy(store.search, store.creations))
 
   return (
     <>
       <NavUtils/>
-      {creations?.length ? (
+      {focus?.filter // filter bar active, list based on filters
+      ? (
         <div className='log-container'>
-          {creations.map((creation) => {
+          {creationsFiltered?.map((creation) => {
             return (
               <StatusLogItem key={creation.id}
                 creation={creation}
@@ -22,8 +25,19 @@ function StatusLog () {
             )
           })}
         </div>
-      )
-        : 'Sorry nothing for you'
+      ) : 'Sorry not yet searchable'
+      
+      // ( // or search bar active, list based on search terms
+      //     <div className='log-container'>
+      //       {creationsSearched?.map((creation) => {
+      //         return (
+      //           <StatusLogItem key={creation.id}
+      //             creation={creation}
+      //             />
+      //         )
+      //       })}
+      //     </div>
+      //   )
       }
     </>
   )
