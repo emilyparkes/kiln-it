936e1145cd13fecb761b51cb893a9921ae57388f
emily parkes <emilycoco@me.com>
2022-09-16 09:55:45 +1200

    new creation styling

--- client/styles/mui_overrides.js
+++ client/styles/mui_overrides.js
@@ -1,19 +1,33 @@
 import { makeStyles } from '@mui/styles'
 
-export const useEditStyles = makeStyles(() => ({
-  root: {
+export const useStyles = makeStyles(() => ({
+  box: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
   },
-  textField: {
+  inputLabel: {
     width: '22ch',
   },
   multiSelect: {
     width: '43.5ch',
   },
-  name: {
+  titleLabel: {
     width: '40ch',
+  },
+  saveButton: {
+    position: 'absolute',
+    color: '#e3c6a4',
+    backgroundColor: '#744F44',
+    right: '18px',
+    width: '95px',
+    bottom: '50px',
+    height: '35px'
+  },
+  MuiFab: {
+    position: 'fixed',
+    bottom: '1rem',
+    right: '2rem',
   },
 }))
