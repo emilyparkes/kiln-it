93bcb21b98de412cf2ebb583a63991b56872d58a
emily parkes <emilycoco@me.com>
2022-09-16 09:54:43 +1200

    add new creation FAB button

--- client/components/StatusLog.jsx
+++ client/components/StatusLog.jsx
@@ -5,41 +5,42 @@ import NavUtils from './nav-utils/NavUtils'
 import StatusLogItem from './StatusLogItem'
 
 import { filterBy, searchBy } from '../client-utils'
+import FloatingAddNew from './FloatingAddNew'
 
-function StatusLog () {
-  const focus = useSelector(store => store.navUtils)
-  const creationsFiltered = useSelector(store => filterBy(store.filter, store.creations))
-  const creationsSearched = useSelector(store => searchBy(store.search, store.creations))
+function StatusLog() {
+  const focus = useSelector((store) => store.navUtils)
+  const creationsFiltered = useSelector((store) =>
+    filterBy(store.filter, store.creations)
+  )
+  const creationsSearched = useSelector((store) =>
+    searchBy(store.search, store.creations)
+  )
 
   return (
     <>
-      <NavUtils/>
-      {focus?.filter // filter bar active, list based on filters
-      ? (
-        <div className='log-container'>
-          {creationsFiltered?.map((creation) => {
-            return (
-              <StatusLogItem key={creation.id}
-                creation={creation}
-                />
-            )
-          })}
-        </div>
-      ) : 
-      // 'Sorry not yet searchable'
-      
-      ( // or search bar active, list based on search terms
-          <div className='log-container'>
-            {creationsSearched?.map((creation) => {
-              return (
-                <StatusLogItem key={creation.id}
-                  creation={creation}
-                  />
-              )
+      <NavUtils />
+      {focus?.filter ? ( // filter bar active, list based on filters
+        <>
+          <div className="log-container">
+            {creationsFiltered?.map((creation) => {
+              return <StatusLogItem key={creation.id} creation={creation} />
             })}
           </div>
-        )
-      }
+
+          <FloatingAddNew />
+        </>
+      ) : (
+        // or search bar active, list based on search terms
+        <>
+          <div className="log-container">
+            {creationsSearched?.map((creation) => {
+              return <StatusLogItem key={creation.id} creation={creation} />
+            })}
+          </div>
+
+          <FloatingAddNew />
+        </>
+      )}
     </>
   )
 }
