37b996a4e8a99ded02cd7487db002ebd09da436f
emily parkes <emilycoco@me.com>
2022-09-23 15:38:57 +1200

    can filter and search gallery

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -1,43 +1,79 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Typography } from '@mui/material'
 
-import { toLowHyphen, filterBy } from '../client-utils'
+import { toLowHyphen, filterBy, searchBy } from '../client-utils'
 
 import Card from './Card'
 import NavUtils from './nav-utils/NavUtils'
 import WaitIndicator from './WaitIndicator'
 
+function Gallery() {
+  const focus = useSelector((store) => store.navUtils)
 
-function Gallery () {
+  const creationsFiltered = useSelector((store) =>
+    filterBy(store.filter, store.creations)
+  )
+  const searchterm = useSelector((store) => store.search)
 
-  const creations = useSelector(store => filterBy(store.filter, store.creations))
+  const creationsSearched = useSelector((store) =>
+    store.search ? searchBy(store.search, store.creations) : store.creations
+  )
 
   return (
     <>
-      <NavUtils/>
-      <WaitIndicator/>
-      {creations && (
-        <div className='card-root'>
-          <div className='card-container'>
-            {creations.map((creation) => {
-              const name = toLowHyphen(creation.name)
-              return (
-                <Link
-                  to={`/creations/${name}`}
-                  key={creation.id}
-                >
-                  <Card
-                    img={'/images/plate.jpeg'}
-                    name={creation.name}
-                    shape={creation.shape}
-                    description={creation.glaze}
-                  />
-                </Link>
-              )
-            })}
+      <NavUtils />
+      <WaitIndicator />
+      {focus?.filter ? (
+        <>
+          <div className="card-root">
+            <div className="card-container">
+              {creationsFiltered.map((creation) => {
+                const name = toLowHyphen(creation.name)
+                return (
+                  <Link to={`/creations/${name}`} key={creation.id}>
+                    <Card
+                      img={'/images/plate.jpeg'}
+                      name={creation.name}
+                      shape={creation.shape}
+                      description={creation.glaze}
+                    />
+                  </Link>
+                )
+              })}
+            </div>
           </div>
-        </div>
+        </>
+      ) : (
+        <>
+          {searchterm && (
+            <Typography
+              variant="h6"
+              sx={{ paddingLeft: '12px', marginBottom: '10px' }}
+            >
+              Search results for &apos;{searchterm}&apos;
+            </Typography>
+          )}
+
+          <div className="card-root">
+            <div className="card-container">
+              {creationsSearched.map((creation) => {
+                const name = toLowHyphen(creation.name)
+                return (
+                  <Link to={`/creations/${name}`} key={creation.id}>
+                    <Card
+                      img={'/images/plate.jpeg'}
+                      name={creation.name}
+                      shape={creation.shape}
+                      description={creation.glaze}
+                    />
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+        </>
       )}
     </>
   )
