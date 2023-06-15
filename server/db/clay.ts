4c7e789e1284881032bd482e5f2149aa6ea0a811
Emily Parkes <emilycoco@me.com>
2023-06-15 14:39:01 +1200

    clay tests CRU passing

--- server/db/clay.ts
+++ server/db/clay.ts
@@ -2,30 +2,23 @@ import connection from './connection'
 
 import { Clay, DBClay } from '../../models/Clay'
 
-export default {
-  getClay,
-  getClayById,
-  addClay,
-  updateClay,
-  deleteClay,
-}
 
-function getClay(db = connection): Promise<Clay[]>{
+export function getClay(db = connection): Promise<Clay[]>{
   return db('clay').select()
 }
 
-function getClayById(id:number, db = connection): Promise<Clay> {
+export function getClayById(id:number, db = connection): Promise<Clay> {
   return db('clay').where('id', id).select().first()
 }
 
-function addClay(clay:Clay, db = connection): Promise<number> {
+export function addClay(clay:Clay, db = connection): Promise<number> {
   return db('clay').insert({ clay })
 }
 
-function updateClay(id:number, clay:Partial<DBClay>, db = connection): Promise<number> {
+export function updateClay(id:number, clay:Partial<DBClay>, db = connection): Promise<number> {
   return db('clay').where('id', id).update(clay)
 }
 
-function deleteClay(id:number, db = connection): Promise<number> {
+export function deleteClay(id:number, db = connection): Promise<number> {
   return db('clay').where('id', id).delete()
 }
