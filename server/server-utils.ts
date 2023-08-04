08396ebeb5668603253b8d2cb8875a4f0d6f709b
Emily Parkes <emilycoco@me.com>
2023-08-04 15:58:24 +1200

    refactor formatter for ts (needs more done)

--- server/server-utils.ts
+++ server/server-utils.ts
@@ -1,6 +1,12 @@
 import _ from 'lodash'
 
-export function prepForDb (body) {
+import { Options, DBOptions } from '../models/Options'
+import { DBGlaze } from '../models/Glaze'
+import { DBClay } from '../models/Clay'
+import { DBShape } from '../models/Shape'
+
+// TODO: Do the reverse of all the below pls
+export function prepForDb (body: Options) {
   if (_.isObject(body)) {
     const newObj = {}
     Object.keys(body).forEach(key => (newObj[_.snakeCase(key)] = body[key]))
@@ -9,17 +15,55 @@ export function prepForDb (body) {
   return body
 }
 
-export function prepForJS (data) {
-  if (_.isObject(data)) {
-    const newObj = {}
-    Object.keys(data).forEach(key => (newObj[_.camelCase(key)] = data[key]))
-    if (_.isArray(newObj.glazes)) prepForJS(newObj.glazes)
-    return newObj
-  } else if (_.isArray(data)) data.map(i => prepForJS(i))
-  return data
+// TODO: starting from here is for client-side :) 
+// TODO: Retest these functions and edit all models to have client state, snake state and camel state
+// prep data for client-side if obj convert as such or if array
+// map then convert using all checks/conversions detailed below
+export function prepforTS (data: DBOptions | DBOptions[]): DBOptions | DBOptions[] {
+  if (isOptionsArray(data)) {
+    return data.map((obj: DBOptions) => snakeToCamelwBools(obj))
+  } else {
+    return snakeToCamelwBools(data)
+  }
 }
 
-// const dataObj = {
+function isOptionsArray(data: DBOptions | DBOptions[]): data is DBOptions[] {
+  return _.isArray(data)
+}
+
+function snakeToCamelwBools (obj: DBOptions) {
+  const newObj = {} as DBOptions
+  // each property in the obj is now in arr, for each property in arr camelcase the key = set value from original (snake) obj
+  // e.g obj = { in_use: 1 } to obj = { inUse: true }
+  Object.keys(obj).forEach(key => newObj[_.camelCase(key) as keyof typeof obj] = obj[key as keyof DBOptions])
+  // this converts the bools from number types back to REAL bools
+  return makeRealBools(newObj)
+}
+
+function makeRealBools (obj: DBOptions) {
+  // check that obj has the "in_use" property to convert value to REAL bool
+ if (isUseable(obj)) {
+  obj.in_use = Boolean(obj.in_use)
+ }
+ // check that obj has the "underglaze" property to convert value to REAL bool
+ if (hasUnderglaze(obj)) {
+  obj.underglaze = Boolean(obj.underglaze)
+ }
+ return obj
+}
+
+// obj is of types known, and should return to be one of specific options that include the key 
+// e.g DBCreation is an option but does not have "in_use"
+function isUseable (obj: DBOptions): obj is DBGlaze | DBClay | DBShape {
+  return 'in_use' in obj
+}
+
+// same as obove but for diff key
+function hasUnderglaze (obj: DBOptions): obj is DBGlaze {
+  return 'underglaze' in obj
+}
+
+// const dataObj = { 
 //   clay: 'White',
 //   id: 1,
 //   date_complete: '2020-07-15T13:45:30',
