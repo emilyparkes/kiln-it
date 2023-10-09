449264d00ab7fc5a00379caae47667eef530dd40
Emily Parkes <emilycoco@me.com>
2023-10-10 12:07:46 +1300

    remove comment from home

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,26 +2,6 @@ import { Link } from 'react-router-dom'
 import { Card, CardMedia, CardContent, Typography } from '@mui/material'
 
 function Home() {
-  // const styles = {
-  //   media: {
-  //     height: '100%',
-  //     paddingTop: '56.25%', // 16:9
-  //   },
-  //   card: {
-  //     position: 'relative',
-  //     height: '94vh',
-  //   },
-  //   overlay: {
-  //     position: 'absolute',
-  //     top: '31vh',
-  //     left: '14vw',
-  //     color: 'rgb(116, 79, 68)',
-  //     backgroundColor: '#e3c6a482',
-  //     borderRadius: '16px',
-  //     width: '312px',
-  //     padding: '10px',
-  //   },
-  // }
   return (
     <>
       <Card sx={{position: 'relative',
